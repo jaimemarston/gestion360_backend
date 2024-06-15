9aefabac5baada0a695b357e505dd457ba48c91f
Elvis Garcia <elviserikvan@gmail.com>
2024-06-15 13:41:25 -0400

    feat(folders): adding validation. Can't create folder in folder's with files

--- src/middleware/folders/check-parent-folder.middleware.js
+++ src/middleware/folders/check-parent-folder.middleware.js
@@ -1,8 +1,14 @@
-import { Folders } from '../../models/index.js';
+import { Folders, MinioFiles } from '../../models/index.js';
 
 const checkParentFolder = async (req, res, next) => {
 
   if (req.body.parent !== null) {
+
+    const filesInFolder = await MinioFiles.findOne({ where: { FolderId: req.body.parent } });
+    if (filesInFolder !== null) {
+      return res.status(400).send({ message: 'No puedes crear una carpeta en otra donde hay archivos' })
+    }
+
 
     const parent = await Folders.findByPk(req.body.parent);
     if (parent === null) {
