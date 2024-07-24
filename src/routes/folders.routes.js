1853b4047506b02301992331911f4c28c60c2cf8
Elvis Garcia <elviserikvan@gmail.com>
2024-07-24 10:51:07 -0400

    feat(folders): removing adming validation

--- src/routes/folders.routes.js
+++ src/routes/folders.routes.js
@@ -25,11 +25,11 @@ const router = Router();
 router.get('/', validarJWT,  getAll);
 router.get('/me', validarJWT, getUserFolders);
 router.get('/:folderId', validarJWT, checkFolderId, getOne);
-router.post('/', validarJWT, checkBody, checkGroupId, checkParentFolder, haveRol('ADMIN_ROLE'), create);
-router.patch('/:folderId',  validarJWT, checkFolderId, checkUpdateBody, haveRol('ADMIN_ROLE'), update);
+router.post('/', validarJWT, checkBody, checkGroupId, checkParentFolder, create);
+router.patch('/:folderId',  validarJWT, checkFolderId, checkUpdateBody, update);
 router.delete('/:folderId', validarJWT, checkFolderId, remove);
 
-router.post('/add-user/:folderId', validarJWT, checkFolderId,  haveRol('ADMIN_ROLE'), addUserToFolder);
-router.post('/remove-user/:folderId', validarJWT, checkFolderId,  haveRol('ADMIN_ROLE'), removeUserToFolder);
+router.post('/add-user/:folderId', validarJWT, checkFolderId,  addUserToFolder);
+router.post('/remove-user/:folderId', validarJWT, checkFolderId, removeUserToFolder);
 
 export default router;
